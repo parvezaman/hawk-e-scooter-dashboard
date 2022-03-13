0a87b567b4573f7efbc3b72c906f8c0d372ff4b7
parvezaman <emailtoamanullah@gmail.com>
2022-03-13 15:42:05 +0600

    just updating everything

---
+++ src/components/Pages/Packages/CreatePackages/CreatePackages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatePackages = () => {
+    return (
+        <div>
+            This is create Packages
+        </div>
+    );
+};
+
+export default CreatePackages;
--- src/components/Pages/Packages/Packages.js
+++ src/components/Pages/Packages/Packages.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CreatePackages from './CreatePackages/CreatePackages';
 
 const Packages = () => {
     return (
-        <div>
-            <h1>Packages or special offers</h1>
-        </div>
+        <>
+            <CreatePackages />
+        </>
     );
 };
 
