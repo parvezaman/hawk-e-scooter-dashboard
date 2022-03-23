3a73cdfd1465c0b76e456f8e3f0c2733daa5c7a4
parvezaman <emailtoamanullah@gmail.com>
2022-03-23 13:06:59 +0600

    package delete option added

--- src/components/Pages/Packages/ShowPackages/ShowPackages.js
+++ src/components/Pages/Packages/ShowPackages/ShowPackages.js
@@ -1,20 +1,41 @@
-import { Typography } from '@mui/material';
-import React from 'react';
-import './ShowPackages.css';
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import "./ShowPackages.css";
 
-const ShowPackages = (allPackages) => {
-    const { packageName, packageDescription, _id } = allPackages.allPackages
-    // console.log(allPackages.allPackages.packageName)
-    return (
-        <div className='packageStyle'>
-            <Typography variant="h3" gutterBottom component="div">
-                {packageName}
-            </Typography>
-            <Typography variant="h6" gutterBottom component="div">
-                {packageDescription}
-            </Typography>
-        </div>
-    );
+const ShowPackages = ({ allPackages }) => {
+  const { packageName, packageDescription, _id } = allPackages;
+  // console.log(allPackages.allPackages.packageName)
+  const handleDelete = (id) => {
+    // console.log("clicked with", id);
+    const proceed = window.confirm("You are going to delete a package!!!");
+    if (proceed) {
+      const url = `https://fast-thicket-64971.herokuapp.com/packages/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount == 1) {
+            alert("1 package has been deleted");
+          }
+        });
+    }
+  };
+  return (
+    <div className="packageStyle">
+      <div className="package-items">
+        <Typography variant="h3" gutterBottom component="div">
+          {packageName}
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+          {packageDescription}
+        </Typography>
+      </div>
+      <div className="package-buttons">
+        <Button onClick={() => handleDelete(_id)}>Delete</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ShowPackages;
