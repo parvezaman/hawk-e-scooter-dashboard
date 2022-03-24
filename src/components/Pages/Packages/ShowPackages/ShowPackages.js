81c066a32718fd844e9af06c114eb0d95af9ad1d
parvezaman <emailtoamanullah@gmail.com>
2022-03-24 15:02:07 +0600

    package style changed

--- src/components/Pages/Packages/ShowPackages/ShowPackages.js
+++ src/components/Pages/Packages/ShowPackages/ShowPackages.js
@@ -1,9 +1,15 @@
 import { Button, Typography } from "@mui/material";
-import React from "react";
+import * as React from "react";
 import "./ShowPackages.css";
+import TestShowPackages from "./testShowPackages";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
 
 const ShowPackages = ({ allPackages }) => {
-  const { packageName, packageDescription, _id } = allPackages;
+  const { packageName, packageDescription, _id, packageImage } = allPackages;
   // console.log(allPackages.allPackages.packageName)
   const handleDelete = (id) => {
     // console.log("clicked with", id);
@@ -22,19 +28,36 @@ const ShowPackages = ({ allPackages }) => {
     }
   };
   return (
-    <div className="packageStyle">
-      <div className="package-items">
-        <Typography variant="h3" gutterBottom component="div">
-          {packageName}
-        </Typography>
-        <Typography variant="h6" gutterBottom component="div">
-          {packageDescription}
-        </Typography>
-      </div>
-      <div className="package-buttons">
-        <Button onClick={() => handleDelete(_id)}>Delete</Button>
-      </div>
-    </div>
+    <Grid item xs={12} md={4} sm={6} lg={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={packageImage}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {packageName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+          <Button
+            color="error"
+            variant="outlined"
+            size="small"
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
